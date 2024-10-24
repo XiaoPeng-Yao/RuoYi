67ad7996e7302790ebb0ecb09c8bec64b973be9c
PC-608 <1627209088@qqcom>
2024-10-24 09:51:13 +0800

    10.24

--- ruoyi-system/src/main/java/TestTop.java
+++ ruoyi-system/src/main/java/TestTop.java
@@ -1,8 +1,14 @@
 public class TestTop {
     public static void main(String[] args){
+        /**
+         * 创建top对象
+         */
         Top topPerson = new Top("xx","20","2");
-      topPerson.java1();
-      topPerson.java2();
-      topPerson.java3();
+        /**
+         * 输出
+         */
+        topPerson.java1();
+        topPerson.java2();
+        topPerson.java3();
     }
 }
