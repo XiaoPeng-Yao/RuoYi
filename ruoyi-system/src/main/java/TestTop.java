22b6c8024d3764129ebd08290374ae133f658137
PC-608 <1627209088@qqcom>
2024-10-24 11:27:43 +0800

    10.24

--- ruoyi-system/src/main/java/TestTop.java
+++ ruoyi-system/src/main/java/TestTop.java
@@ -1,20 +1,26 @@
 public class TestTop {
     public static void main(String[] args){
+
         /**
          * 创建top对象
          */
+
         Top topPerson = new Top("xx","20","2");
+
         /**
          * 输出
          */
+
         topPerson.name();
         topPerson.age();
         topPerson.ranking();
 
         Top topPer = new Top("cc","21","1");
+
         /**
          * 输出
          */
+
         topPer.name();
         topPer.age();
         topPer.ranking();
--- ruoyi-system/src/main/java/Top.java
+++ ruoyi-system/src/main/java/Top.java
@@ -1,25 +1,34 @@
 /**
  * 表示排名的类
  */
+
 public class Top {
+
     /**
      * 名字
      */
+
     private String name;
+
     /**
      *年龄
      */
+
     private String age;
+
     /**
      * 名次
      */
+
     private String ranking;
+
     /**
      * 构造法
      * @param name
      * @param age
      * @param ranking
      */
+
     public Top(String name,String age,String ranking){
         this.name=name;
         this.age=age;
@@ -29,6 +38,7 @@ public class Top {
     /**
      * 获取名字
      */
+
     public void name() {
         System.out.println("名字: " + name);
     }
@@ -36,6 +46,7 @@ public class Top {
     /**
      * 获取年龄
      */
+
     public void age() {
         System.out.println("年龄: " + age);
     }
@@ -43,12 +54,15 @@ public class Top {
     /**
      * 获取名次
      */
+
     public void ranking() {
         System.out.println("名次: " + ranking);
     }
+
     /**
      * 获取名字
      */
+
     public void name1() {
         System.out.println("名字: " + name);
     }
@@ -56,6 +70,7 @@ public class Top {
     /**
      * 获取年龄
      */
+
     public void age1() {
         System.out.println("年龄: " + age);
     }
@@ -63,6 +78,7 @@ public class Top {
     /**
      * 获取名次
      */
+
     public void ranking1() {
         System.out.println("名次: " + ranking);
     }
