f99eb3e2ecd9f0cca5536e596854a93574df7e7d
PC-608 <1627209088@qq.com>
2024-10-28 09:58:39 +0800

    接口练习

---
+++ ruoyi-system/src/main/java/Book.java
@@ -0,0 +1,6 @@
+/**
+ * 定义一个表示书本类型的接口
+ */
+public interface Book {
+    String name();
+}
---
+++ ruoyi-system/src/main/java/History.java
@@ -0,0 +1,10 @@
+/**
+ * 实现Book接口的历史类
+ */
+public class History implements Book {
+    @Override
+    public String name() {
+        return "历史";
+    }
+
+}
